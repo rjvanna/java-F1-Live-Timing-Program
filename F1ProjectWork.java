3f0702881440c500c39844bfb32097bd0f51dd84
EthanElder <ElderEt@jbu.edu>
2026-05-05 21:57:38 -0500

    Add files via upload

--- F1ProjectWork.java
+++ F1ProjectWork.java
@@ -104,32 +104,21 @@ class Racer {
 
     }
 
-class Race {
+static class Race {
     private static int getPointsForPlace(int place) {
-        switch (place) {
-            case 1:
-                return 25;
-            case 2:
-                return 18;
-            case 3:
-                return 15;
-            case 4:
-                return 12;
-            case 5:
-                return 10;
-            case 6:
-                return 8;
-            case 7:
-                return 6;
-            case 8:
-                return 4;
-            case 9:
-                return 2;
-            case 10:
-                return 1;
-            default:
-                return 0;
-            }
+        return switch (place) {
+            case 1 -> 25;
+            case 2 -> 18;
+            case 3 -> 15;
+            case 4 -> 12;
+            case 5 -> 10;
+            case 6 -> 8;
+            case 7 -> 6;
+            case 8 -> 4;
+            case 9 -> 2;
+            case 10 -> 1;
+            default -> 0;
+        };
         }
 
     public static void runRace(ArrayList<Racer> racers) {
